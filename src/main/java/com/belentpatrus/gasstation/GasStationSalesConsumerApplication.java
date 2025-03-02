e6fdea15ec862e2358b7d5741ab8ce7157cfb803
Belent <belentpatrus@hotmail.com>
2025-03-01 18:42:36 -0800

    can parse the excel sheet

--- src/main/java/com/belentpatrus/gasstation/GasStationSalesConsumerApplication.java
+++ src/main/java/com/belentpatrus/gasstation/GasStationSalesConsumerApplication.java
@@ -15,7 +15,7 @@ public class GasStationSalesConsumerApplication {
         MerchandiseItemSaleExcelReader myService = context.getBean(MerchandiseItemSaleExcelReader.class);
 
         // Use the bean directly
-        myService.readProductsFromExcel("C:/Users/billy/Documents/Daily Merchandise Item Sales Reports/test.xls");
+        myService.readProductsFromExcel("C:/Users/belent/Documents/Daily Merchandise Item Sales Reports/test.xls");
     }
 
 }
