6531a9615f35daa4e9ad95ee215ddb2b5f683b8b
Belent Patrus <belentpatrus@hotmail.com>
2025-03-18 14:20:05 -0400

    renamed controller

--- src/main/java/com/belentpatrus/gasstation/controller/DailyMerchandiseSalesController.java
+++ src/main/java/com/belentpatrus/gasstation/controller/DailyMerchandiseSalesController.java
@@ -1,55 +1,49 @@
 package com.belentpatrus.gasstation.controller;
 
 
-import com.belentpatrus.gasstation.model.dailysales.DailyMerchandiseSales;
-import com.belentpatrus.gasstation.model.dailysales.MerchandiseItemSale;
 import com.belentpatrus.gasstation.model.dailysales.enums.Department;
 import com.belentpatrus.gasstation.service.DailyMerchandiseSalesSummaryService;
-import com.belentpatrus.gasstation.service.MerchandiseItemSaleExcelReaderService;
 import com.belentpatrus.gasstation.service.SyncService;
 import com.belentpatrus.gasstation.service.dto.DailyMerchandiseSalesSummaryDTO;
 import com.belentpatrus.gasstation.service.dto.SyncDailyMerchandiseSalesAndProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:5173")
-@RestController("/consumer")
-public class MerchandiseItemSaleExcelReaderController {
+@RequestMapping("/consumer")
+@RestController()
+public class DailyMerchandiseSalesController {
 
 
     private DailyMerchandiseSalesSummaryService dailyMerchandiseSalesSummaryService;
     private SyncService syncService;
 
     @Autowired
-    public MerchandiseItemSaleExcelReaderController(DailyMerchandiseSalesSummaryService dailyMerchandiseSalesSummaryService, SyncService syncService) {
+    public DailyMerchandiseSalesController(DailyMerchandiseSalesSummaryService dailyMerchandiseSalesSummaryService, SyncService syncService) {
         this.dailyMerchandiseSalesSummaryService = dailyMerchandiseSalesSummaryService;
         this.syncService = syncService;
 
     }
 
-    @GetMapping("/consumer/{date}")
+    @GetMapping("/{date}")
     public DailyMerchandiseSalesSummaryDTO getSummary(@PathVariable String date) {
         LocalDate localDate = LocalDate.parse(date);
         return dailyMerchandiseSalesSummaryService.getDailyMerchandiseSales(localDate);
     }
 
-    @GetMapping("/consumer/{id}/{department}")
+    @GetMapping("/{id}/{department}")
     public double getSummary(@PathVariable long id, @PathVariable Department department) {
         return dailyMerchandiseSalesSummaryService.getTotalSoldByDepartment(id, department);
     }
 
-    @GetMapping("/consumer/getsummary/{id}")
+    @GetMapping("/getsummary/{id}")
     public DailyMerchandiseSalesSummaryDTO getSummary(@PathVariable long id) {
         return dailyMerchandiseSalesSummaryService.getDailyMerchandiseSalesSummary(id);
     }
 
-    @GetMapping("/consumer/sync/{date}")
+    @GetMapping("/sync/{date}")
     public SyncDailyMerchandiseSalesAndProductDTO returnNotSynced(@PathVariable String date) {
         LocalDate localDate = LocalDate.parse(date);
         DailyMerchandiseSalesSummaryDTO dailyMerchandiseSalesSummaryDTO = dailyMerchandiseSalesSummaryService.getDailyMerchandiseSales(localDate);
